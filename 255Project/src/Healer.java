fd267167c12084a91430bffc630d6ef53987862b
shrutia898 <118574196+shrutia898@users.noreply.github.com>
2024-04-05 17:23:13 -0400

    Update Healer.java

--- 255Project/src/Healer.java
+++ 255Project/src/Healer.java
@@ -1,1 +1,6 @@
-
+public class Healer extends Player{
+     public Healer(String name, Boolean isAlive, int daysSurvived, int mana, int health){
+		super(name, isAlive, daysSurvived, mana, health);
+	}	
+    
+}
