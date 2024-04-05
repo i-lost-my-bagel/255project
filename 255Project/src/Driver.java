3024d3ba04ea2daf3dfa447f68a1bde9bfc0115d
i_lost_my_bagel <lily@lilysthings.org>
2024-04-05 17:14:46 -0400

    added berserker to driver

--- 255Project/src/Driver.java
+++ 255Project/src/Driver.java
@@ -20,13 +20,21 @@ public class Driver {
 	int randomNumber = rand.nextInt(max - min) + min;
         
         Scanner scanner = new Scanner(System.in); 
-        System.out.print("Enter name: ");
+        System.out.print("Enter Guardian Name: ");
+        Guardian guardian = new Guardian(scanner.nextLine(), true, 0, 100, 200); 
         
-        Guardian testplayer = new Guardian(scanner.nextLine(), true, 0, 100, 200); 
+        System.out.print("Enter Berserker Name: ");
+        Berserker berserker = new Berserker(scanner.nextLine(), true, 0, 100, 100); 
         
-        System.out.println("Name: " + testplayer.getName());
-        System.out.println("Health: " + testplayer.getHealth());
-        System.out.println("Mana: " + testplayer.getMana());
+        System.out.println("Guardian Stats\n");
+        System.out.println("Name: " + guardian.getName());
+        System.out.println("Health: " + guardian.getHealth());
+        System.out.println("Mana: " + guardian.getMana());
+        
+        System.out.println("Berserker Stats\n");
+        System.out.println("Name: " + berserker.getName());
+        System.out.println("Health: " + berserker.getHealth());
+        System.out.println("Mana: " + berserker.getMana());
 
     }
 }
