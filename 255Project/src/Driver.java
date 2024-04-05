af9ec890e97c9c50712a1fec4c737253f45a7d1c
i_lost_my_bagel <lily@lilysthings.org>
2024-04-05 16:34:54 -0400

    driver please work

---
+++ 255Project/src/Driver.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lily
+ */
+public class Driver {
+    public static void main(String args[ ])
+    {
+	Random rand = new Random();
+	int min = 1;
+	int min = 4
+	int randomNumber = rand.nextInt(max - min) + min;
+	return randomNumber;
+    }
+}
