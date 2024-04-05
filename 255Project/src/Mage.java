3255cc7c7fc0c10fc628d4d0a9b9808488f63fcb
i_lost_my_bagel <lily@lilysthings.org>
2024-04-05 17:16:00 -0400

    fixed mage

--- 255Project/src/Mage.java
+++ 255Project/src/Mage.java
@@ -1,4 +1,4 @@
-public class Mage {
+public class Mage extends Player{
      public Mage(String name, Boolean isAlive, int daysSurvived, int mana, int health){
 		super(name, isAlive, daysSurvived, mana, health);
 	}	
