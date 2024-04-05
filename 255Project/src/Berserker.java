0e2379ad6be2ed4b3857afe4f732795e7094b4ec
i_lost_my_bagel <lily@lilysthings.org>
2024-04-05 17:08:52 -0400

    added berserker

---
+++ 255Project/src/Berserker.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lily
+ */
+public class Berserker extends Player
+{
+  public Berserker(String name, Boolean isAlive, int daysSurvived, int mana, int health){
+		super(name, isAlive, daysSurvived, mana, health);
+	}	
+}
