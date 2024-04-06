24c4b9cb66dcc3dcdae052ba0fb259651db34a1b
shrutia898 <118574196+shrutia898@users.noreply.github.com>
2024-04-06 13:39:08 -0400

    Create Minion.java

---
+++ 255Project/src/Minion.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author shrutiatitkar
+ */
+public class Minion extends Enemy {
+    public Minion(Boolean isAlive, int health, String name){
+        super(isAlive, health, name);
+    }
+}
