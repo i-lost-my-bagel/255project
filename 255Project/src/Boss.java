43ad2c5abd6298b853a4eb9a651075cae6fad07f
shrutia898 <118574196+shrutia898@users.noreply.github.com>
2024-04-06 13:56:13 -0400

    Create Boss.java

---
+++ 255Project/src/Boss.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author shrutiatitkar
+ */
+public class Boss extends Enemy {
+    public Boss(String name, Boolean isAlive, int health, int mana){
+        super(name, isAlive, health, mana);
+    }
+   
+}
