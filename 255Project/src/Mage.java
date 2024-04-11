3b8f31f7769f18e14a7abdbb3f6d424d44b44683
Quinn <164554088+qgchambe@users.noreply.github.com>
2024-04-11 01:27:57 -0400

    Update Mage.java

--- 255Project/src/Mage.java
+++ 255Project/src/Mage.java
@@ -1,6 +1,6 @@
 public class Mage extends Player{
-     public Mage(String name, Boolean isAlive, int daysSurvived, int mana, int health){
-		super(name, isAlive, daysSurvived, mana, health);
+     public Mage(String name, boolean isAlive, int daysSurvived, int health, int mana, int damage, boolean isShielded){
+		super(name, isAlive, daysSurvived, health, mana, damage, isShielded);
 	}	
     
 }
