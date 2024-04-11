871dbaa526922e7b4d0b7c171ad0b004adf4d043
Quinn <164554088+qgchambe@users.noreply.github.com>
2024-04-11 01:27:09 -0400

    Update Guardian.java

--- 255Project/src/Guardian.java
+++ 255Project/src/Guardian.java
@@ -1,11 +1,10 @@
 public class Guardian extends Player
 {
-  public Guardian(String name, Boolean isAlive, int daysSurvived, int mana, int health){
-		super(name, isAlive, daysSurvived, mana, health);
+  public Guardian(String name, boolean isAlive, int daysSurvived, int health, int mana, int damage, boolean isShielded){
+		super(name, isAlive, daysSurvived, health, mana, damage, isShielded);
 	}
 	//private double getHealth()
 	//{
 	//	return super.getHealth() + 100;
 	//}
-	
 }
